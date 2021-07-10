026c68504e6fcafe56a92584e720cbc7ec1aa28a
yang <qdwds94@gmail.com>
2021-07-10 21:37:12 +0800

    配置：mongoose

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*
  * @Description: 
  * @Date: 2021-07-07 10:54:16
- * @LastEditTime: 2021-07-09 10:50:43
+ * @LastEditTime: 2021-07-10 21:32:48
  */
 const Koa = require('koa');
 const app = new Koa();
@@ -13,8 +13,14 @@ const routes = require('./routes');
 const koaJWT = require("koa-jwt");
 const { jwtScrentKey } = require("./utils/jwt/secretKey");
 require("./ws");
+
+const mongoose = require("./db")
 // error handler
 onerror(app);
+
+//  mongoose
+mongoose();
+
 
 // middlewares
 app.use(bodyparser({
@@ -56,6 +62,5 @@ app.on('error', (err, ctx) => {
 });
 
 //  websocket
-
 
 module.exports = app
